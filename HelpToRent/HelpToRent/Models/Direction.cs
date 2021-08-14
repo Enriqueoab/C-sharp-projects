cc3702695948718ae7ace6ab9eee42ac5b87326b
Enrique Barca <barcaenrique7@gmail.com>
2021-08-14 20:01:39 +0100

    Creating a new class direction

---
+++ HelpToRent/HelpToRent/Models/Direction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelpToRent.Models
+{
+    public class Direction
+    {
+        public Direction()
+        {
+        }
+
+        public Direction(int houseId, string street, string town, string city)
+        {
+            Id = houseId;
+            Street = street;
+            Town = town;
+            City = city;
+        }
+
+        public int Id { get; set; }
+        public String Street { get; set; }
+        public String Town { get; set; }
+        public String City { get; set; }
+
+    }
+}

af73541a3bedf8687c822ba8573afe0188af80e8
Enrique Barca <barcaenrique7@gmail.com>
2021-08-14 15:04:21 +0100

    Create the new obj bill releated to the obj House by its id

---
+++ HelpToRent/HelpToRent/Models/Bill.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelpToRent.Models
+{
+    public class Bill
+    {
+        public Bill()
+        {}
+
+        public Bill(int houseId, bool allBillsIncluded, string billComment)
+        {
+            this.AllBillsIncluded = allBillsIncluded;
+            this.BillComment = billComment;
+            this.Id = houseId;
+        }
+
+        public Bill(bool allBillsIncluded, int houseId)
+        {
+            this.AllBillsIncluded = allBillsIncluded;
+            this.Id = houseId;
+        }
+
+
+        public Boolean AllBillsIncluded { get; set; }
+        public String BillComment { get; set; }
+        public int Id { get; set; }
+    }
+}
+        
